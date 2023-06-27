8fa13396d2063a88ed13c19dcd5fc84145bf8470
Emmanuel Vargas <126733106+EmmanuelV22@users.noreply.github.com>
2023-06-27 16:17:16 +0000

    done

--- src/js/views/VehicleDetails.jsx
+++ src/js/views/VehicleDetails.jsx
@@ -26,7 +26,7 @@ export const VehicleDetails = () => {
 
 			{JSON.stringify(store.oneVehicle) !== '{}' ?(
 			<div className="card text-start mx-auto"
-				style={{ width: "60%" }} key="index">
+				style={{ width: "80%" }} key="index">
 				<img src={imageSource} onError={handleImageError} className="card-img-top mx-auto mt-2" style={{ height: "18rem", objectFit: "cover", borderRadius: "20px",width: "65%" }} alt="..." />
 				<div className="card-body" style={{ height: "13rem", overflow: "scroll" }}>
 					<h5 className="card-title">Name: {store.oneVehicle.result.properties.name} </h5>
